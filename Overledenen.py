071b08f5474f842e1e2e26c820b8c496f5d5eab2
heesters <bheesters@gmail.com>
2023-02-03 14:49:55 +0100

    huh

--- Overledenen.py
+++ Overledenen.py
@@ -211,4 +211,4 @@ num_frames = len(df_circle)
 
 anim = mpl.animation.FuncAnimation(fig, animate, init_func=init, frames=num_frames, interval=50, blit=True) 
 anim.save('sterfte_anim.gif', writer= PillowWriter(fps=50) , dpi=72)
-anim.save('sterfte_anim.mp4', writer='ffmpeg', dpi=300, extra_args=['-vf', 'tpad=stop_mode=clone:stop_duration=5'])
+#anim.save('sterfte_anim.mp4', writer='ffmpeg', dpi=300, extra_args=['-vf', 'tpad=stop_mode=clone:stop_duration=5'])
